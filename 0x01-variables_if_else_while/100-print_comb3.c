aca763b8a15ee286c057557ee127dfa8e59c71df
FiliRama <phionhkg@gmail.com>
2023-06-16 19:39:12 -0700

    optional

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+* main -  execution starts at this function
+*
+* Return: 0- Success
+*/
+
+int main()
+{
+int i = '0';
+int j = '0';
+for(j = '0'; j <= '9'; j++)
+   for(i ='0'; i <= '9'; i++)
+       if((i != j) && (j <= i)){
+	      putchar(j);
+	      putchar(i);
+            if((i != '9' ) || ( j != '8')){
+		putchar(',');
+		putchar(' ');
+		   }
+		}
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/**
+* main -  execution starts at this function
+*
+* Return: 0- Success
+*/
+
+
+int main()
+{
+int k = '0';
+int j = '0';
+int i ='0';
+for(i = '0';i <= '9';i++)
+   for(j = '0';j <= '9';j++)
+      for(k ='0';k <= '9';k++)
+        if(!((k == j) || (j > k) || (i == j) || (i > j))){
+	      putchar(i);
+	      putchar(j);
+	      putchar(k);
+            if(!(k == '9' && j == '8' && i == '7')){
+		putchar(',');
+		putchar(' ');
+		   }
+		}
+putchar('\n');
+return (0);
+
+}
