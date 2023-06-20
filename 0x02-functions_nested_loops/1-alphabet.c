68c88578f674d5121f064dbb0d5d54d8151f4b43
FiliRama <phionhkg@gmail.com>
2023-06-20 10:56:55 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_alphabet - print alphabet
+*
+* Return: 0 success
+*/
+void print_alphabet(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+_putchar(alphabet);
+_putchar('\n');
+return (0);
+}
