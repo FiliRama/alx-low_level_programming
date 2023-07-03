fc517f71ca01d6e40f46a8d413edd70e474b9650
FiliRama <phionhkg@gmail.com>
2023-07-03 00:46:11 -0700

    advanced tasks

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* set_string - to set pointer value to char
+* @s: the pointer to be set
+* @to: input string
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -17,5 +17,5 @@ char *_strpbrk(char *s, char *accept)
 				return (s);
 		s++;
 	}
-	return (NULL);
+	return (0);
 }
