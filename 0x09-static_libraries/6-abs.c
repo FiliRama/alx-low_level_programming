264a007546e386e9acb2038a89cbd099edb4590e
FiliRama <phionhkg@gmail.com>
2023-07-08 06:07:43 -0700

    task2

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _abs - absolute value of an integer
+* @n: argument of the function
+* Return: the absolute value of int
+*/
+int _abs(int n)
+{
+if (n < 0)
+return (-n);
+else
+return (n);
+}
